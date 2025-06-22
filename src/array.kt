9412778a221352905ddb66c500a2356dbd559b67
Quema100 <myoon0929@gmail.com>
2025-06-23 04:20:29 +0900

    array

---
+++ src/array.kt
@@ -0,0 +1,19 @@
+fun main() {
+//    val a = emptyArray<String>() // 원소 0개
+//    val b = arrayOf("hello", "world") // 원소 2개
+//    val c = arrayOf(1,4,9) // 원소 3개
+//
+//    println(a.contentToString()) // []
+//    println(b.contentToString()) // [hello, world]
+//    println(c.contentToString()) // [1, 4, 9]
+//
+//    val size = readLine()!!.toInt() // !! <- null로 인한 오류 예외처리
+//    val squ = Array(size) {(it + 1)*(it + 1)} // it이 index를 하나 봄
+//
+//    println(squ.contentToString()) // input: 1, output: [1] {contentToString()이거는 배열 출력할때 쓰는거 같음}
+
+//    val operation = charArrayOf('+', '-', '*', '/', '&') // char은 ''으로 감싸야되나 봄
+//    val squ = IntArray(10) {(it + 1)*(it + 1)} // ()속에 넣은 숫자가 for 문처럼 사용되나 봄
+//    println(operation.contentToString()) // [+, -, *, /, &]
+//    println(squ.contentToString()) // [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+}
